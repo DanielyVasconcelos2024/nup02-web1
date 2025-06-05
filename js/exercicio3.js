ad95ce2d7998b00c3661d4bc850989d42643bca2
Letz007 <leuxavieraraujo007@gmail.com>
2025-06-05 12:26:19 -0300

    Create exercicio3.js

---
+++ js/exercicio3.js
@@ -0,0 +1,17 @@
+function calcularElevacaoQuadratica() {
+    const numero = document.getElementById("inputNumero").value;
+    const numeroStr = numero.toString();
+
+    let resultado = "";
+
+    for (let i = 0; i < numeroStr.length; i++) {
+        const digito = parseInt(numeroStr[i]);
+        if (!isNaN(digito)) {
+            const quadrado = digito * digito;
+            resultado += quadrado.toString();
+        }
+    }
+
+    document.getElementById("outputOriginal").innerText = numero;
+    document.getElementById("outputQuadradoConcatenado").innerText = parseInt(resultado);
+}
