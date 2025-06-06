ec0d34b939691756d5fdd43567d0155bc00e980a
luna santos <oluna8466@gmail.com>
2025-06-06 12:12:04 -0300

    Updating menu and changing names

--- assets/js/menu-exercicios.js
+++ assets/js/menu-exercicios.js
@@ -1,17 +1,18 @@
 function renderMenuExercicios() {
     const isIndex = window.location.pathname.endsWith('/index.html') || window.location.pathname === '/';
+    const prefix = isIndex ? 'pages/exercicios/' : '';
 
-    const prefix = isIndex ? '/pages/exercicios/' : '';
-
-    const exercicios = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 25, 26
+    const arquivos = [
+        2, 3, 4, 5, 6, 8, 9, 10,
+        11, 12, 13, 14, 16, 17, 18, 19, 20,
+        21, 22, 25, 26, 29, 30
     ];
 
     let links = '';
-    exercicios.forEach(num => {
-        links += `<a href="${prefix}exercicio${num}.html">Exercício ${num}</a>\n`;
+    arquivos.forEach((realNum, index) => {
+        const visualNum = index + 1; 
+        const filename = `desafio${realNum}.html`;
+        links += `<a href="${prefix}${filename}">Exercício ${visualNum}</a>\n`;
     });
 
     const menu = `
@@ -22,6 +23,13 @@ function renderMenuExercicios() {
             </div>
         </div>
     `;
-    document.getElementById('menu-exercicios').innerHTML = menu;
+
+    const container = document.getElementById('menu-exercicios');
+    if (container) {
+        container.innerHTML = menu;
+    } else {
+        console.error('Elemento com id "menu-exercicios" não encontrado no HTML.');
+    }
 }
+
 window.addEventListener('DOMContentLoaded', renderMenuExercicios);
