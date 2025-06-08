554a7a56fb8295fd75f767840127e3c499ea9a51
luna santos <oluna8466@gmail.com>
2025-06-08 20:47:27 -0300

    Removing - Challenges.js 1 and 7, Challenge.html 30

--- js/exercicio1.js
+++
@@ -1,6 +0,0 @@
-function calcularMedia(...numeros) {
-    if (numeros.length === 0) return 0;
-    
-    let soma = numeros.reduce((acc, valor) => acc + valor, 0);
-    return soma / numeros.length;
-}
