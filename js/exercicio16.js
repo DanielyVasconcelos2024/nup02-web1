370f0065153be378048899688db59d38cb68a562
Carol <anakfig2016@gmail.com>
2025-06-09 08:07:59 -0300

    in the exercise 16, now its possible to differentiate upperCase from lowerCase

--- js/exercicio16.js
+++ js/exercicio16.js
@@ -1,42 +1,53 @@
-function getText(){
+function getText() {
     return document.getElementById('inputStringCifrada').value;
 }
 
-function getShifts(){
-    return document.getElementById('inputChave').value;
+function getShifts() {
+    return parseInt(document.getElementById('inputChave').value); 
 }
 
-function getDisplayText(){
+function getDisplayText() {
     return document.getElementById("outputStringOriginal");
 }
 
-function getDisplayShift(){
+function getDisplayShift() {
     return document.getElementById('outputChave');
 }
 
-function getDisplayResult(){
+function getDisplayResult() {
     return document.getElementById('outputDescriptografada');
 }
 
-function ceaser(){
-    let  alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+function isUpper(str) {
+    return str === str.toUpperCase() && str !== str.toLowerCase();
+}
+
+function ceaser() {
+    let alfabeto = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+        'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
 
     let texto = getText();
     let texto_final = "";
-    let shift = getShifts() * -1;
+    let shift = getShifts() * -1; 
 
-    for (let letra of texto.toLowerCase()){
-        if (alfabeto.includes(letra)){
-            let posicao_letra = alfabeto.indexOf(letra);
+    for (let letra of texto) {
+        if (alfabeto.includes(letra.toLowerCase())) {
+            let posicao_letra = alfabeto.indexOf(letra.toLowerCase());
             let nova_posicao = (posicao_letra + shift) % 26;
 
-            if(nova_posicao < 0){
+            if (nova_posicao < 0) {
                 nova_posicao += 26;
             }
 
-            texto_final += alfabeto[nova_posicao];
-        }
-        else {
+            let letra_final = alfabeto[nova_posicao];
+
+            if (isUpper(letra)) {
+                letra_final = letra_final.toUpperCase();
+            }
+            texto_final += letra_final;
+        } else {
             texto_final += letra;
         }
     }
@@ -45,22 +56,21 @@ function ceaser(){
     let display_shift = getDisplayShift();
     let display_result = getDisplayResult();
 
-    if(display_texto){
+    if (display_texto) {
         display_texto.textContent = texto;
     } else {
         console.error(`Elemento com ID: 'outputStringOriginal' não foi encontrado`);
     }
 
-    if(display_shift){
+    if (display_shift) {
         display_shift.textContent = getShifts();
     } else {
-        console.error(`Elemento com ID: 'outputChave' não foi encontrado`)
+        console.error(`Elemento com ID: 'outputChave' não foi encontrado`);
     }
 
-    if(display_result){
+    if (display_result) {
         display_result.textContent = texto_final;
     } else {
-        console.error(`Elemento com ID: 'outputDescriptografada' não foi encontrado`)
+        console.error(`Elemento com ID: 'outputDescriptografada' não foi encontrado`);
     }
 }
-
